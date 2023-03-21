37c21e38a2980cd72fc68257e9fcd1edab027158
moomint8 <office05053@gmail.com>
2023-03-21 17:12:28 +0900

    23/03/21 17:10 AOP

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect // AOP 선언
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")    // 적용 대상
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START : " + joinPoint.toString());
+        try {
+            return joinPoint.proceed(); // 해당 코드 실행(인라인 적용 된 코드)
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
+
